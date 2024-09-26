4009b753074241e1aa41f50ce83de951de0375f8
pensamientocomputacional-clases <keimer71018@gmail.com>
2024-09-26 12:01:41 -0500

    Add files via upload

---
+++ data/slide174.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{B22CB069-9900-47A0-AE95-8F09ED1316AB}'];loadHandler&&loadHandler(173, '<div id="spr0_38ed8317"><div id="spr1_38ed8317" class="kern slide"><img id="img0_38ed8317" src="data/img288.png" width="1075px" height="604px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_38ed8317" class="kern slide"></div></div>', '{"s":[]}');})();
